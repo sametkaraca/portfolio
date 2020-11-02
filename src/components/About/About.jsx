aa05b46e64cd7a044caf2ed12ca9f7e890bac3eb
Samet <samet.karaca.no@gmail.com>
2020-11-02 22:43:25 +0100

    About section is compelted for tablet and desktop (Crude Version)

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ export const About = () => {
         <AboutHeader />
         <AboutSummary />
         <AboutEducation />
-        <AboutHobbies />
         <AboutTravel />
+        <AboutHobbies />
     </>);
 }
--- src/components/About/AboutHeader.jsx
+++ src/components/About/AboutHeader.jsx
@@ -7,8 +7,8 @@ export const AboutHeader = () => {
             <div className={style.about_header_container}>
                 <div className={style.about_header_container_title}>
                     <span className={style.topline}></span>
-                    <h2 className={style.about_header_container_title__text}>About</h2>
-                    <h4 className={style.about_header_container_title__content}>Hey,<br /> <br /> I'm Samet. <br /> A frontend engineer.</h4>
+                    <h1 className={style.about_header_container_title__text}>About</h1>
+                    <h2 className={style.about_header_container_title__content}>Hey,<br /> <br /> I'm Samet. <br /> A frontend engineer.</h2>
                 </div>
             </div>
         </section>);
