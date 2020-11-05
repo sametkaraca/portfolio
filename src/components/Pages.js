342fce3de3e34ed984ac36d5354b5c4a9211ed64
Samet <samet.karaca.no@gmail.com>
2020-11-05 09:23:35 +0100

    Router update.

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -9,11 +9,11 @@ import { Lab } from "./Lab/Lab";
 export const Pages = () => {
   return (
     <Switch>
-      <Route path="/About" component={About} exact />
-      <Route path="/Project" component={Project} exact />
-      <Route path="/Lab" component={Lab} exact />
-      <Route path="/Home" component={Home} exact />
-      <Route path="/" component={Home} exact />
+      <Route path="portfolio/About" component={About} exact />
+      <Route path="portfolio/Project" component={Project} exact />
+      <Route path="portfolio/Lab" component={Lab} exact />
+      <Route path="portfolio/Home" component={Home} exact />
+      <Route path="portfolio/" component={Home} exact />
       <Route component={Error} exact />
     </Switch>
   );
