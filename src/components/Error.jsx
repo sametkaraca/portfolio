c6c12b539268fe6fe2f25f829297fc87e3ea93ed
Samet <samet.karaca.no@gmail.com>
2020-11-07 20:24:55 +0100

    Optimization v1.0 Footer,Navbar,manifest.json, Error page have been cleaned up.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import style from "../styles/main.module.scss";
+import style from "../styles/components/_error.module.scss";
 
 export const Error = () => {
   return (
     <section className={style.error}>
       <div className={style.error_container}>
         <h1 className={style.error_container__title}>404</h1>
-        <h5 className={style.error_container__content}>
-          I can’t seem to find the page you’re looking for
-        </h5>
       </div>
     </section>
   );
